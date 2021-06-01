7f336bd473c172540abe776dab2639c3fd51ae98
rhaynes3k <rhaynes3k@gmail.com>
2021-05-31 20:39:07 -0500

    Auto count feature added, like button

--- js/challenge.js
+++ js/challenge.js
@@ -1,21 +1,35 @@
 var ctr = document.querySelector("#counter")
 const ctrPlus = document.querySelector("#plus")
 const ctrMinus = document.querySelector("#minus")
+const like = document.getElementById("heart")
+const lkLst = document.getElementsByTagName("ul")
+var likeCnt = 0
 var counter = 0
 let ctrP = function (e){
     counter++
     ctr.innerText = counter
     console.log(ctr)
   }
-document.addEventListener("DOMContentLoaded", function(e){
-  window.setInterval(ctrP, 1200)
-})
-
-ctrPlus.addEventListener("click", ctrP)
-
-ctrMinus.addEventListener("click", function (){
+let ctrM = function (e){
     counter--
     ctr.innerText = counter
     console.log(ctr)
+  }
+  let likeList = function (){
+    likeCnt++
 
+    lkLst.appendChild()
+  }
+//document.addEventListener("DOMContentLoaded", function(e){
+  //window.setInterval(ctrP, 1000)
+//})
+ctrPlus.addEventListener("click", ctrP)
+ctrMinus.addEventListener("click", ctrM)
+
+like.addEventListener('click', function(){
+  likeCnt++
+  let newLi = document.createElement('li')
+  newLi.innerText = `${ctr.innerText} has been liked ${likeCnt} times`
+  lkLst.innerHTML = newLi
+  console.log(likeCnt, lkLst.innerHTML)
 })
